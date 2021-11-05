30145281cdb40da55efb0d0e9503ef3343c45cf0
vrodedanya <socurahlem@yandex.ru>
2021-11-05 08:47:05 +0300

    Fixed warnings

--- source/HasDataType.cpp
+++ source/HasDataType.cpp
@@ -9,18 +9,18 @@ void morph::HasDataType::setData(Mandatory type, const std::string& name, std::s
 }
 
 // Returns -1 if '=' isn't in line
-static std::size_t getKey(const std::string& line, const std::string& key, std::string& keyBuffer)
+static int getKey(const std::string& line, const std::string& key, std::string& keyBuffer)
 {
 	std::size_t begin = line.find(key);
 	std::size_t end = line.find('=');
 	if (begin == std::string::npos || end == std::string::npos) return -1;
 	keyBuffer = line.substr(begin, end - begin);
 	keyBuffer.erase(std::remove(keyBuffer.begin(), keyBuffer.end(), ' '), keyBuffer.end());
-	return end;
+	return static_cast<int>(end);
 }
 
 // Return -1 if '"' aren't in line
-static std::size_t getValue(const std::string& line, std::string& value, std::size_t position)
+static int getValue(const std::string& line, std::string& value, std::size_t position)
 {
 	std::size_t valueBegin = line.find('"', position) + 1;
 	std::size_t valueEnd  = line.find('"', valueBegin + 1);
@@ -29,11 +29,11 @@ static std::size_t getValue(const std::string& line, std::string& value, std::si
 		return -1;
 	}
 	value = line.substr(valueBegin, valueEnd - valueBegin);
-	return valueEnd;
+	return static_cast<int>(valueEnd);
 }
 
 // Return -1 if line ends
-static std::size_t reduceLine(std::string& line)
+static int reduceLine(std::string& line)
 {
 	std::size_t valueType = line.find(',');
 	if (valueType == std::string::npos)
@@ -54,12 +54,12 @@ void morph::HasDataType::getData(const std::string& line)
 		while (bufferLine.size() > elem.first.size())
 		{
 			std::string key;
-			std::size_t keyEnd = getKey(bufferLine, elem.first, key);
+			int keyEnd = getKey(bufferLine, elem.first, key);
 			if (keyEnd == -1) break;
 			if (elem.first == key)
 			{
 				std::string value;
-				std::size_t valueEnd = getValue(bufferLine, value, keyEnd);
+				int valueEnd = getValue(bufferLine, value, keyEnd);
 				if (valueEnd == -1) throw std::runtime_error("getData: Bad string format for " + key);
 				*elem.second.data = value;
 				isInited = true;
